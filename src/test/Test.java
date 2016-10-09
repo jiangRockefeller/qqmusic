7aad07f339063da418a2eae4c80f60be5abdd111
jiangyongjie <1978078602@qq.com>
2016-10-09 21:46:35 +0800

    delete

--- src/test/Test.java
+++ src/test/Test.java
@@ -9,11 +9,8 @@ import com.bdqn.qqmusic.service.RecordService;
 public class Test {
 	public static void main(String[] args) {
 		 RecordService test=new RecordService();
-
-		 List<Record> list=null;
-		 
-			 list=test.getAllRecord();
-	
+		 List<Record> list=null;		 
+			 list=test.getAllRecord();	
 		 for (Record record : list) {
 		 System.out.println(record.getRmainartist()+record.getRname()+record.getRdate());
 		 }
