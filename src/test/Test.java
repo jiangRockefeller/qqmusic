652edbc523200973d3e9410acf5032b6a9266656
jiangyongjie <1978078602@qq.com>
2016-10-09 16:16:16 +0800

    test

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.bdqn.qqmusic.pojo.Record;
 import com.bdqn.qqmusic.service.RecordService;
 //test
+//this is my test
 public class Test {
 	public static void main(String[] args) {
 		 RecordService test=new RecordService();
