246d79cfddebe89aea4c6737cd84e06f4c8547e8
GahooLuk <929070533@qq.com>
2016-10-12 15:11:18 +0800

    AlbumCo.java

--- src/com/bdqn/qqmusic/servlet/AlbumCo.java
+++ src/com/bdqn/qqmusic/servlet/AlbumCo.java
@@ -3,7 +3,6 @@ package com.bdqn.qqmusic.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -16,7 +15,22 @@ import com.bdqn.qqmusic.pojo.Song;
 import com.bdqn.qqmusic.service.RecordService;
 import com.bdqn.qqmusic.service.SongService;
 
-public class AlbumServlet extends HttpServlet {
+public class AlbumCo extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public AlbumCo() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
 
 	/**
 	 * The doGet method of the servlet. <br>
@@ -28,47 +42,40 @@ public class AlbumServlet extends HttpServlet {
 	 * @throws ServletException if an error occurred
 	 * @throws IOException if an error occurred
 	 */
+	
+	
+	
+	
+	
+	
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		
 		PrintWriter out = response.getWriter();
 		
 	
 		
-		RecordService recordService=new RecordService();
-		List<Record> mod_playlist=new ArrayList<Record>();
-		mod_playlist=recordService.getAllRecord();
-		System.out.println("hah");
-		for (Record record : mod_playlist) {
-			System.out.println(record.getRname()+" "+record.getRdate());
-		}
-		request.getSession().setAttribute("all", mod_playlist);
-		response.sendRedirect("../qqmusic/albumContext.jsp");//重定向 2次转发
-		out.flush();//输出来
-		out.close();//关闭
+
 		
 		
-//		
-//		//陆嘉豪写的作用域--这个是取歌曲名字
-//		   SongService songService=new SongService();
-//		   List<Song> albumContext=new ArrayList<Song>();
-//		  
-//		   
-//		   albumContext=songService.getAllSongs();
-//		   System.out.println("嘻嘻");
-//		   for(Song song:albumContext){
-//			   System.out.println(song.getSname()+song.getArtist());			   
-//		   }
-//		   request.getSession().setAttribute("name",albumContext);
-//		   response.sendRedirect("../qqmusic/albumContext.jsp");
-//		   out.flush();
-//		   out.close();
+		//陆嘉豪写的作用域--这个是取歌曲名字
+		   SongService songService=new SongService();
+		   List<Song> albumContext=new ArrayList<Song>();
+		  
+		   
+		   albumContext=songService.getAllSongs();
+		   System.out.println("嘻嘻");
+		   for(Song song:albumContext){
+			   System.out.println(song.getSname()+song.getArtist());			   
+		   }
+		   request.getSession().setAttribute("name",albumContext);
+		   response.sendRedirect("../qqmusic/albumContext.jsp");
+		   out.flush();
+		   out.close();
 		
 	}
+	
 
-	
-	
 	/**
 	 * The doPost method of the servlet. <br>
 	 *
@@ -95,6 +102,15 @@ public class AlbumServlet extends HttpServlet {
 		out.println("</HTML>");
 		out.flush();
 		out.close();
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
 	}
 
 }
