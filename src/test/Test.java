fa9be2d7cd139cf7c4c8c49d108c092d40ba1f07
k4kentchow <529497710@qq.com>
2016-10-12 13:37:08 +0800

    Update Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,15 +1,22 @@
 package test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.bdqn.qqmusic.pojo.Record;
-import com.bdqn.qqmusic.pojo.Song;
+import com.bdqn.qqmusic.pojo.SongListManage;
+import com.bdqn.qqmusic.pojo.SongListManageDAO;
+import com.bdqn.qqmusic.pojo.User;
 import com.bdqn.qqmusic.service.RecordService;
-import com.bdqn.qqmusic.service.SongService;
 
 public class Test {
 	public static void main(String[] args) {
+		User user=new User();
+		user.setUid(2);
+		SongListManageDAO test=new SongListManageDAO();
+		System.out.println(test.getFavList(user).getSmname());
+		
+		
+		
 		SongService song = new SongService();
 		List<Song> list = new ArrayList<Song>();
 		list = song.getAllSongs();
@@ -17,6 +24,20 @@ public class Test {
 		for(int i=0;i<list.size();i++ )
 		System.out.println(list.get(i).getSname() + " \tArtist： " + list.get(i).getArtist().getAname());
 		
-		 
+		
+		
+		
+		
+		//record表getAll遍历
+//		RecordService test = new RecordService();
+//		List<Record> list = null;
+//		list = test.getAllRecord();
+//		for (Record record : list) {
+//			System.out.println(record.getRmainartist() + record.getRname()
+//					+ record.getRdate());
+//		}
+	
+	
+	
 	}
 }
